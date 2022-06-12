f98805752fcf0e0f8526e1f556b56c05ba186956
MaksimKayumov <81908868+MaksimKayumov@users.noreply.github.com>
2022-06-12 23:10:09 +0500

    Add files via upload

---
+++ sego.bat
@@ -0,0 +1,11 @@
+@echo off
+set v=%%
+echo @echo off>%1.bat
+	FOR /F "delims=; tokens=1,2,3,4,5" %%a in (%1) do (
+		echo :%%a>>%1.bat
+		echo set %%b=%%c>>%1.bat
+		echo if %v%%%b%v% == false goto %%d>>%1.bat
+
+	)
+)
+echo Program Compiled.
